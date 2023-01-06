b5d8fd268c23abde93a7aa4c167baa2fc8e5b9ce
flare-s <webflarez@gmail.com>
2023-01-06 12:45:05 +0000

    Fixed typo on post template

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,7 +2,7 @@
     <article>
             <h1>{{ $post->title }}</h1>
             <p>{!! $post->body !!}</p>
-            <div>By <a href="authors/{{$post->author->name}}">{{$post->author->name}}</a> in <a href="categories/{{$post->category->slug}}">{{ $post->category->name }}</a></div>
+            <div>By <a href="authors/{{$post->author->username}}">{{$post->author->name}}</a> in <a href="categories/{{$post->category->slug}}">{{ $post->category->name }}</a></div>
             <div>
             <a href="/">Go back</a>
             </div>
