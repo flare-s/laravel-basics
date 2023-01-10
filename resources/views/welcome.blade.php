5f1d18f2c803da4b018a24460637e7a7306f1324
flare-s <webflarez@gmail.com>
2023-01-10 10:34:13 +0000

    Dynamic Home page feature post

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,17 +13,19 @@
     @endforeach --}}
     @include ('_header')
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-feature-post />
+        <x-feature-post :post="$posts[0]" />
 
         <div class="lg:grid lg:grid-cols-2">
-            <x-post-card />
-            <x-post-card />
+            @foreach ($posts->skip(1) as $post)
+                <x-post-card :post="$post" />
+            @endforeach
+            
         </div>
 
-        <div class="lg:grid lg:grid-cols-3">
+        {{-- <div class="lg:grid lg:grid-cols-3">
             <x-post-card />
             <x-post-card />
             <x-post-card />
-        </div>
+        </div> --}}
     </main>
 </x-layout>
