08b9bfee84b103baa49aaee2d9b12fdade684363
flare-s <webflarez@gmail.com>
2023-02-18 23:09:58 +0000

    filter and search connected together

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -15,7 +15,7 @@
     </x-slot>
         <x-dropdown-item href="/" :active="!$currentCategory">all</x-dropdown-item>
         @foreach ($categories as $category)
-            <x-dropdown-item href="/?category={{ $category->slug }}" :active="$currentCategory && $category->is( $currentCategory)">{{ $category->name }}</x-dropdown-item>
+            <x-dropdown-item href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category')) }}" :active="$currentCategory && $category->is( $currentCategory)">{{ $category->name }}</x-dropdown-item>
             {{-- <x-dropdown-item href="/categories/{{ $category->slug }}" :active="isset($currentCategory) && $currentCategory->is($category)">{{ $category->name }}</x-dropdown-item> --}}
         @endforeach
 </x-dropdown>
--- resources/views/posts/_header.blade.php
+++ resources/views/posts/_header.blade.php
@@ -58,10 +58,14 @@
 
         <!-- Search -->
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
-            <form method="GET" action="#">
+            <form method="GET" action="">
                 <input type="text" name="search" placeholder="Find something"
                 value="{{ request('search') }}"
                        class="bg-transparent placeholder-black font-semibold text-sm">
+
+                @if(request('category')) 
+                    <input type="hidden" name="category" value="{{ request('category') }}" />
+                @endif
             </form>
         </div>
     </div>
