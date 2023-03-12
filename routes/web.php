07ea0c133b14f2612063f9058ccc756ce8229df7
flare-s <webflarez@gmail.com>
2023-03-12 13:16:51 +0000

    Add comments to posts

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
-
+use App\Http\Controllers\PostCommentsController;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
@@ -28,9 +28,15 @@ Route::middleware('guest')->group(function() {
     Route::get('/register', [RegisterController::class, 'create']);
 
     Route::post('/register', [RegisterController::class, 'store']);
-    Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
-    Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
+    Route::get('/login', [SessionsController::class, 'create']);
+    Route::post('/login', [SessionsController::class, 'store']);
 });
+
+Route::middleware('auth')->group(function() {
+    Route::post('/posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
+});
+
+
 
 
 
